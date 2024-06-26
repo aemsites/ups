55616db7c3f6b7179164be35ae8b0b8d5e6cd9e8
Sean Steimer <ssteimer@adobe.com>
2024-06-26 09:00:59 -0700

    add tealium to see if we can load martech earlier on fast connection

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { sampleRUM, loadScript } from './aem.js';
+
+const loadDelayed = async () => {
+  loadScript('https://tags.tiqcdn.com/utag/ups/yoda/prod/utag.sync.js');
+};
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadDelayed();
